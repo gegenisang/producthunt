f9cee4f8f2c336e2517deb0b4a48f4881a0580c6
gegenisang <gegenisang@gmail.com>
2017-09-29 13:02:29 +0800

    complete

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -1,13 +1,13 @@
 $("#sidemenu li>a").on("click", function () {
   console.log('click');
   $(this).parent().siblings().removeClass("active");
-  $(this).parent().toggleClass('active');
+  $(this).parent().addClass('active');
   $(this).next().toggle();
   return false;
 });
 
 $("#submenu li").on("click", function () {
   $("#submenu li").removeClass("active");
-  $(this).addClass("active");
+  $(this).toggleClass("active");
   return false;
 });
